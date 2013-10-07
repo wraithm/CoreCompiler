18653ed79a2409b758c6e3ec87474bc402da46c2
Matthew Wraith <wraithm@gmail.com>
2013-10-06 19:52:09 -0500

    Converted globals from Map to dictionary in order to maintain scope.

--- src/TiState.hs
+++ src/TiState.hs
@@ -3,12 +3,16 @@ module TiState where
 import qualified Data.Map.Lazy as M
 import Data.List (mapAccumL, foldl)
 import Data.Monoid ((<>))
+import Data.Maybe (fromMaybe)
 
 import AST
 import Heap
 import CorePrelude
 
 -- This is a simple template instantiation graph reducer
+
+-- Simple helper tool for list dictionaries
+findWithDefault x k = fromMaybe x . lookup k
 
 data TiState = TiState
     { stack :: TiStack
@@ -21,7 +25,7 @@ data TiState = TiState
 data TiDump = TiDump deriving Show
 type TiStack = [Addr]
 type TiHeap = Heap Node
-type TiGlobals = M.Map Name Addr
+type TiGlobals = [(Name, Addr)]
 type TiStats = Int
 
 data Node = NApp Addr Addr -- Application
@@ -49,9 +53,8 @@ compile program = TiState
     , stats = 0 } 
   where
     combDefs = program ++ preludeDefs
-    (initialHeap, gs) = buildInitialHeap combDefs
-    initialGlobals = M.fromList gs
-    addrOfMain = M.findWithDefault 0 "main" initialGlobals
+    (initialHeap, initialGlobals) = buildInitialHeap combDefs
+    addrOfMain = findWithDefault 0 "main" initialGlobals
 
 tiFinal :: TiState -> Bool
 tiFinal (TiState [a] _ h _ _) = isDataNode $ hLookup h a
@@ -77,19 +80,18 @@ cStep (TiState s d h g st) name args body = TiState s' d h' g st
     s' = resultAddr : drop (length args + 1) s
     (h', resultAddr) = instantiate body h env
     env = argBindings <> g
-    argBindings = mapZip args (getArgs h s)
-    mapZip xs ys = foldl (\a (k,v) -> M.insert k v a) M.empty (zip xs ys)
+    argBindings = zip args (getArgs h s)
 
 getArgs h (s:st) = map getArg st
     where getArg addr = let (NApp _ arg) = hLookup h addr in arg
 
-instantiate :: CoreExpr -> TiHeap -> M.Map Name Addr -> (TiHeap, Addr)
+instantiate :: CoreExpr -> TiHeap -> [(Name, Addr)] -> (TiHeap, Addr)
 instantiate (Num n) h env = alloc h (NNum n)
 instantiate (App e1 e2) h env = alloc h2 (NApp a1 a2)
   where
     (h1, a1) = instantiate e1 h env
     (h2, a2) = instantiate e2 h1 env
-instantiate (Var v) h env = (h, M.findWithDefault err v env)
+instantiate (Var v) h env = (h, findWithDefault err v env)
     where err = error $ "Undefined name " ++ show v ++ " in " ++ show env
 instantiate (Constr tag arity) h env = error "Can't instantiate constructors... yet..."
 instantiate (Let isRec defs body) h env = error "Can't instantiate lets... yet..."
