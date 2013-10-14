da328f8f48d9fc288655068b22c43e2f170babaf
Matthew Wraith <wraithm@gmail.com>
2013-10-14 16:03:27 -0500

    Added lambda lift test.

---
+++ src/LLTest.hs
@@ -0,0 +1,26 @@
+module LLTest where
+
+import GraphReduction.Compiler (runProgram, compileComb)
+
+import Core.LambdaLift (lambdaLift, freeVars, abstract, rename)
+import Core.Parser (parseProgram)
+import Core.Pretty (prettyProgram)
+
+-- prog = "true x y = x; if p x y = p x y; main = letrec f = \\x. if true 3 (f 3) in f 3;"
+prog = "main = let true = \\x y. x; false = \\x y. y; if = \\p x y. p x y in if true 3 4;"
+
+main = do
+    let parsed = parseProgram prog
+        lifted = lambdaLift parsed
+    printPretty parsed
+    break
+    printPretty (step1 parsed)
+    break
+    printPretty (step2 parsed)
+    break
+    printPretty lifted
+  where
+    break = putStrLn "--------------------------"
+    printPretty = putStrLn . prettyProgram
+    step1 = abstract . freeVars
+    step2 = rename . step1
